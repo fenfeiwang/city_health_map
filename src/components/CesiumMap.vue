c44bea050c3994f93b6efd3de148c9bf41ac714d
Jenny Yan <yunxuan.yan@gmail.com>
2019-04-08 09:22:58 +1200

    get points from csv file and draw them on map

--- src/components/CesiumMap.vue
+++ src/components/CesiumMap.vue
@@ -3,7 +3,9 @@
 </template>
 
 <script>
-
+import http from 'vue-resource';
+import Vue from 'vue';
+Vue.use(http);
 
 export default {
     name: 'CesiumMap',
@@ -14,24 +16,60 @@ export default {
         viewer.imageryLayers.addImageryProvider(new Cesium.BingMapsImageryProvider({
             url : 'https://dev.virtualearth.net',
             key : 'AoP-LhJzZplGyzyekYwMdZ_U8V_J-6Cr2fRvOoQjU3lbNMApYha5J9P6D-7Kc-eY',
-            mapStyle : Cesium.BingMapsStyle.CANVAS_GRAY
-        }));
+            mapStyle : Cesium.BingMapsStyle.CANVAS_LIGHT
+        })); //Cesium.BingMapsStyle.CANVAS_GRAY  Cesium.BingMapsStyle.CANVAS_LIGHT Cesium.BingMapsStyle.CANVAS_DARK
         viewer.camera.setView({
             destination: Cesium.Cartesian3.fromDegrees(116.3974, 39.9342, 20000)
         });
-        let a = this.addPoint(Cesium.Cartesian3.fromDegrees(116.2895356,40.00400583),viewer,Cesium);
+        // this.addPoint(Cesium.Cartesian3.fromDegrees(116.2895356,40.00400583),viewer,Cesium);
+        this.$http.get(`${process.env.BASE_URL}gps_date_time_lon_lat.csv`)
+        .then(response=>{
+            let gpsObj = this.csv2json(response.body);
+            let a = 0;
+            gpsObj.forEach(data=>{
+                if(data && data.Longitude_NTU && data.Latitude_NTU){
+                    this.addPoint(Cesium.Cartesian3.fromDegrees(data.lon,data.lat),viewer,Cesium.Color.FUCHSIA);
+                }
+                a++;
+             })
+        })
 //        viewer.zoomTo(a);
     },
     methods:{
-        addPoint(position,viewer,Cesium) {
+        addPoint(position,viewer,color) {
             return viewer.entities.add({
                 point:{
-                    color:Cesium.Color.FUCHSIA,
+                    color:color,
                     pixelSize:4,
                     show:true
                 },
                 position:position
             });
+        },
+        csv2json(csv) {
+            var lines=csv.replace(/\"/g,"").split("\n");
+            var result = [];
+            var headers=lines[0].split(",");
+            
+            for(var i=1;i<lines.length;i++){
+                var obj = {};
+                var currentline=lines[i].split(",");
+                for(var j=1;j<headers.length-1;j++){
+                    obj[headers[j]] = currentline[j];
+                }
+                obj["Latitude"] = currentline[headers.length-1];
+                obj["lat"] = this.gps2lonlat(obj["Latitude_NTU"])+0.001410833;
+                obj["lon"] = this.gps2lonlat(obj["Longitude_NTU"])+0.006158333;
+                result.push(obj);
+            }
+            console.log(result[0]);
+            return result;
+        },
+        gps2lonlat(num) {
+            let i = num/100;
+            let a = Math.floor(i);
+            let b = i - Math.floor(i);
+            return a + b*100/60;
         }
     }
 }
@@ -42,4 +80,4 @@ export default {
   width: 100%;
   height: 100%;
 }
-<style>
+</style>
